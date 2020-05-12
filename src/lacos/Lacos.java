a6fc48787819c0870d2be1b029e047d7c09c1f09
Jeferson Leon <jefersonleonblue@gmail.com>
2020-05-12 20:49:13 -0300

    Foi inserido a idade e fiz um controle de maiores de idade

--- src/lacos/Lacos.java
+++ src/lacos/Lacos.java
@@ -9,10 +9,12 @@ import javax.swing.JOptionPane;
 public class Lacos {
 
     public static void main(String[] args) {
-        int c, sexo, contF, contM;
-        c = 0;      contF = 0;     contM = 0;
-        while (c < 10) {
+        int c, sexo, contF, contM, idade, cont18;
+        c = 0;      contF = 0;     contM = 0; cont18=0; 
+        
+        while (c < 5) {
             sexo = Integer.parseInt(JOptionPane.showInputDialog("Digite 1 Feminino ou 2 Masculino"));
+            idade = Integer.parseInt(JOptionPane.showInputDialog("Digite a idade:"));
             if (sexo == 1) {
                 contF++;
             } else if (sexo == 2) {
@@ -21,9 +23,12 @@ public class Lacos {
                 c--;//contador de decremento
                 JOptionPane.showMessageDialog(null,"Usuário, digite apenas 1 ou 2");
             }
+            if(idade>=18){
+                cont18++;
+            }
             c++;
         }//fim do loop while
-        JOptionPane.showMessageDialog(null,"Quant. Mulheres: "+contF+"\nQuant. Homens:"+contM);
+        JOptionPane.showMessageDialog(null,"Quant. Mulheres: "+contF+"\nQuant. Homens:"+contM+"\nQuant. de maiores de idade:"+cont18);
 
     }
 
